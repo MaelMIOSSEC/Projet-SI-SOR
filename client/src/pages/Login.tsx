be89ae6e30fe8392c1aee83f9b0cda02e7e7cf7c
MaelMIOSSEC <mama.miossec@gmail.com>
2026-03-10 13:14:27 +0100

    Update client/Login.tsx

--- client/src/pages/Login.tsx
+++ client/src/pages/Login.tsx
@@ -1,5 +1,9 @@
-import { useEffect, useState } from "react";
 
 export default function Login() {
-    
+    return (
+        <main id="content">
+            <h1>📊 Real-time polls</h1>
+        <p>Click on a poll below to participate.</p>
+        </main>
+    );
 }
