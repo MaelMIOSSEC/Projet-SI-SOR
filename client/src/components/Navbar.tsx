838539f59f419c5156992b5dfc71cc1b499cf724
MaelMIOSSEC <mama.miossec@gmail.com>
2026-03-12 13:25:19 +0100

    Update

--- client/src/components/Navbar.tsx
+++ client/src/components/Navbar.tsx
@@ -1,74 +1,107 @@
-import React from "react";
+import { useEffect, useState } from "react";
+import { Link } from "react-router";
+import { useAuth } from "../hooks/useAuth.ts";
+import { useNavigate } from "react-router";
 
-const Navbar: React.FC = () => {
+const Navbar = () => {
+  const navigate = useNavigate();
+  const { user } = useAuth();
+
+  const [isShrunk, setIsShrunk] = useState(false);
+
+  const isConnected = user !== null;
+
+  useEffect(() => {
+    const handleScroll = () => {
+      if (window.scrollY === 0) {
+        setIsShrunk(false);
+      } else {
+        setIsShrunk(true);
+      }
+    };
+
+    window.addEventListener("scroll", handleScroll);
+    return () => window.removeEventListener("scroll", handleScroll);
+  }, []);
+
   return (
-    <>
-      <nav className="navbar navbar-expand-lg bg-body-tertiary">
-        <div className="container-fluid">
-          <a className="navbar-brand" href="#">
-            Navbar
-          </a>
-          <button
-            className="navbar-toggler"
-            type="button"
-            data-bs-toggle="collapse"
-            data-bs-target="#navbarNavDropdown"
-            aria-controls="navbarNavDropdown"
-            aria-expanded="false"
-            aria-label="Toggle navigation"
-          >
-            <span className="navbar-toggler-icon" />
-          </button>
-          <div className="collapse navbar-collapse" id="navbarNavDropdown">
-            <ul className="navbar-nav">
-              <li className="nav-item">
-                <a className="nav-link active" aria-current="page" href="#">
-                  Home
-                </a>
-              </li>
-              <li className="nav-item">
+    <nav
+      className={`navbar navbar-expand-lg navbar-dark fixed-top ${isShrunk ? "navbar-shrink" : ""}`}
+      id="mainNav"
+    >
+      <div className="container">
+        <Link className="navbar-brand" to="/">
+          StackBan
+        </Link>
+
+        <button
+          className="navbar-toggler"
+          type="button"
+          data-bs-toggle="collapse"
+          data-bs-target="#navbarResponsive"
+          aria-controls="navbarResponsive"
+          aria-expanded="false"
+          aria-label="Toggle navigation"
+        >
+          Menu
+          <i className="fas fa-bars ms-1"></i>
+        </button>
+
+        <div className="collapse navbar-collapse" id="navbarResponsive">
+          <ul className="navbar-nav text-uppercase ms-auto py-4 py-lg-0">
+            <li className="nav-item drop-down-menu">
+              <a className="nav-link" style={{ fontSize: 17 }} href="#">
+                Mes tableaux
+              </a>
+              <div className="subpage">
                 <a className="nav-link" href="#">
-                  Features
+                  Présentation
                 </a>
-              </li>
-              <li className="nav-item">
-                <a className="nav-link" href="#">
-                  Pricing
+                <a className="nav-link" href="#propos">
+                  A Propos
                 </a>
-              </li>
-              <li className="nav-item dropdown">
+                <a className="nav-link" href="#contact">
+                  Contact
+                </a>
+              </div>
+            </li>
+
+            {isConnected ? (
+              <li className="nav-item drop-down-menu">
                 <a
-                  className="nav-link dropdown-toggle"
-                  href="#"
-                  role="button"
-                  data-bs-toggle="dropdown"
-                  aria-expanded="false"
+                  className="nav-link"
+                  style={{ fontSize: 17 }}
+                  href="Services/services.html"
                 >
-                  Dropdown link
+                  Profil
                 </a>
-                <ul className="dropdown-menu">
-                  <li>
-                    <a className="dropdown-item" href="#">
-                      Action
-                    </a>
-                  </li>
-                  <li>
-                    <a className="dropdown-item" href="#">
-                      Another action
-                    </a>
-                  </li>
-                  <li>
-                    <a className="dropdown-item" href="#">
-                      Something else here
-                    </a>
-                  </li>
-                </ul>
               </li>
-            </ul>
-          </div>
+            ) : (
+              <>
+                <li className="nav-item drop-down-menu">
+                  <a
+                    className="nav-link"
+                    style={{ fontSize: 17 }}
+                    href="/login"
+                  >
+                    Connexion
+                  </a>
+                </li>
+                <li className="nav-item">
+                  <a
+                    className="nav-link"
+                    style={{ fontSize: 17 }}
+                    href="/register"
+                  >
+                    Inscription
+                  </a>
+                </li>
+              </>
+            )}
+          </ul>
         </div>
-      </nav>
-    </>
+      </div>
+    </nav>
   );
 };
 
