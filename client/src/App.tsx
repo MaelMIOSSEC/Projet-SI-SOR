f94a59839d5c2ad1eec91e16d03f4e2397155f52
MaelMIOSSEC <mama.miossec@gmail.com>
2026-03-12 10:44:01 +0100

    Add NavBar

--- client/src/App.tsx
+++ client/src/App.tsx
@@ -3,11 +3,13 @@ import { BrowserRouter, Route, Routes } from "react-router";
 import Login from "./pages/Login";
 import Register from "./pages/Register";
 import Index from "./pages/Index";
+import Navbar from "./components/Navbar";
 import "./App.css";
 
 function App() {
   return (
     <BrowserRouter>
+      <Navbar />
       <Routes>
         <Route path="/" element={<Index />} />
         <Route path="/register" element={<Register />} />
--- client/src/pages/Index.tsx
+++ client/src/pages/Index.tsx
@@ -1,3 +1,5 @@
 export default function Index() {
-    
+    return (
+        <h1>SALUT</h1>
+    );
 }
