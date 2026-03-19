bc234d2ac1f4b65f1c58dfdc0ea92ba3971dd2db
kerzil-hugo <hugokerzil@gmail.com>
2026-03-19 14:15:03 +0100

    Correction fichier vite.config.ts pour le deno check

--- client/vite.config.ts
+++ client/vite.config.ts
@@ -1,15 +1,17 @@
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
 import path from 'node:path' // Ajout du préfixe "node:"
+import { fileURLToPath } from 'node:url'
+
+const __dirname = path.dirname(fileURLToPath(import.meta.url))
 
 // https://vite.dev/config/
 export default defineConfig({
   plugins: [react()],
   resolve: {
     alias: {
-      // On utilise import.meta.dirname à la place de __dirname
-      'react': path.resolve(import.meta.dirname, './node_modules/react'),
-      'react-dom': path.resolve(import.meta.dirname, './node_modules/react-dom'),
+      'react': path.resolve(__dirname, './node_modules/react'),
+      'react-dom': path.resolve(__dirname, './node_modules/react-dom'),
     },
   },
 })
